ac0c9ae6d489bce894163bebebff5dbc4fc65a9d
evildani <evil.dani@gmail.com>
2014-11-14 17:04:39 -0500

    Solved Array indexes for bindings

--- nsconf2html.pl
+++ nsconf2html.pl
@@ -12,12 +12,12 @@ sub extract_params{
 		my %params;
         my @params_temp = split('-',$line);
         my @arr;
-        print "+++++++++ Dump split en - arreglo:\n".Dumper(@params_temp)."\n+++++++++++++arreglo fin -------- \n";
+        #print "+++++++++ Dump split en - arreglo:\n".Dumper(@params_temp)."\n+++++++++++++arreglo fin -------- \n";
 	for my $elem (@params_temp){
 		@arr = split(/ /, $elem);
   		$params{$arr[0]} = $arr[1];
   		#print "\n Key ".$arr[0]." Value ".$arr[1]."\n";
-  		print "==========Dump sub split en ' ':\n".Dumper(@arr)."\n ========= arreglo fin --------------- \n";
+  		#print "==========Dump sub split en ' ':\n".Dumper(@arr)."\n ========= arreglo fin --------------- \n";
   	}
   	
   	return %params;
@@ -80,6 +80,50 @@ print $out "</table><br><br>\n";
 close $info;
 open $info, $file or die "Could not open $file: $!";
 
+
+print "Virtual Server - Services:\n";
+#first pass to detect services bound to virtual servers 
+while( my $line = <$info>)  {   
+   
+    if($line =~ /bind lb vserver/){
+        my @values = split(' ',$line);
+        if(exists $bindings{$values[3]}){
+        	my @svcs = @{$bindings{$values[3]}};
+        	push @svcs,$values[4];
+    		$bindings{$values[3]} = \@svcs;
+    	}else{
+    		my @svcs;
+    		push @svcs,$values[4];
+    		$bindings{$values[3]} = \@svcs;
+    	}
+        #print $values[3]." - ".$values[4]."\n";
+    }
+    
+}
+
+print $out "<table border=1pt><tr><td>Virtual Server Name</td><td>Service Name</td></tr>\n";
+print "VS services bindings:\n";
+#print "=======================\n".Dumper(%bindings)."\n++++++++++++++++++++";
+for (keys %bindings){
+     print " Key:  ".$_." \n";
+     my @value = @{$bindings{$_}};
+     #print "=====\n".Dumper(@value)."======\n";
+     print $out "<tr><td rowspan=".scalar @value.">".$_."</td>"; 
+     for my $i (0 .. $#value){
+     #for my $val (@value){
+     	#print "      Val en i ".$i.": ".$value[$i]."\n";
+     	if($i==0){ 
+     	print "      Val en i ".$i.": ".$value[$i]."\n";
+     	print "<tr>"; 
+     	}else{
+     		print "      Val en i ".$i.": ".$value[$i]."\n";
+     	}
+     	print $out "<td>".$value[$i]."</td></tr>";
+     }
+}
+print $out "</table><br><br>\n";
+open $info, $file or die "Could not open $file: $!";
+
 print "Virtual Server List:\n";
 print $out "<table border=1pt><tr><td>Virtual Server Name</td><td>Category</td><td>Value</td></tr>\n";
 #first pass to detect virtual servers
@@ -89,50 +133,42 @@ while( my $line = <$info>)  {
         my @values = split(' ',$line);
         my %params = extract_params($line);
     	$vserver{ $values[3] } = $line;
+    	my @services =  $bindings{$values[3]};
+    	#print "Dump arr".Dumper(@services)."\n";
+
+    	#print "==============\n".Dumper(@services)."\n+++++++\n";
         print $values[3]."\n";
-        print $out "<tr><td rowspan=6>".$values[3]."</td><td>Tipo</td><td>".$values[4]."</td></tr>";
+        print $out "<tr><td rowspan=6>".$values[3]."</td><td>Tipo</td><td>".$values[4]."</td></tr>\n";
         print $out "<tr><td>IP</td><td>".$values[5]."</td></tr>\n";
         print $out "<tr><td>Puerto</td><td>".$values[6]."</td></tr>\n";
     	############## Add aditional lines if you need more rows with information, 
     	############## params is a hash that uses the key as the -param i.e. -persistenceType without the '-' 
     	print $out "<tr><td>persistenceType</td><td>".$params{"persistenceType"}."</td></tr>\n";
-    	print $out "<tr><td>Persistence Timeout</td><td>".$params{"timeout"}."</td></tr>\n";
-    	print $out "<tr><td>Loadbalance Method</td><td>".$params{"lbmethod"}."</td></tr>\n";
+    	if(exists $params{"timeout"})
+    	{ 
+    	 	print $out "<tr><td>Persistence Timeout</td><td>".$params{"timeout"}."</td></tr>\n";
+    	 }else{
+    	 	print $out "<tr><td>Persistence Timeout</td><td></td></tr>\n";
+    	 }
+    	if(exists $params{"lbmethod"})
+    	{ 
+    		print $out "<tr><td>Loadbalance Method</td><td>".$params{"lbmethod"}."</td></tr>\n";
+    	}else{
+    		print $out "<tr><td>Loadbalance Method</td><td>ROUNDROBIN</td></tr>\n";
+    	}
+    	
+    	for my $i (0 .. @services-1){
+    		print "++++++++++\n".Dumper(@services)."\n==========";
+    		if ($i==0){ print $out "<tr><td>Services</td rowspan=".@services."><td>".$services[$i]."</td></tr>\n";
+    		}else{
+    			print $out "<tr><td>".$services[$i]."</td></tr>\n";	
+    		}
+    	}
     	#print "Dump hash".Dumper(\%params)."\n";
     #persistenceType
     #timeout
     #cltTimeout
     }
-}
-print $out "</table><br><br>\n";
-open $info, $file or die "Could not open $file: $!";
-
-print "Virtual Server - Services:\n";
-#first pass to detect services bound to virtual servers 
-while( my $line = <$info>)  {   
-   
-    if($line =~ /bind lb vserver/){
-        my @values = split(' ',$line);
-        if(exists $bindings{$values[3]}){
-    		$bindings{$values[3],push($bindings{$values[3]},$values[4])};
-    	}else{
-    		$bindings{$values[3]} = [$values[4]];
-    	}
-        #print $values[3]." - ".$values[4]."\n";
-    }
-}
-
-print $out "<table border=1pt><tr><td>Virtual Server Name</td><td>Service Name</td></tr>\n";
-#print VS services bindings:
-while ((my $key,my $value) = each(%bindings)){
-     print $key." \n";
-     print $out "<tr><td rowspan=".scalar @$value.">".$key."</td>"; 
-     my $first = 0;
-     for my $val (@$value){
-     	print "      ".$val."\n";
-     	if($first!=0){ print $out "<tr>"; }
-     	print $out "<td>".$val."</td></tr>";
-     }
 }
 print $out "</table><br><br>\n";
 
