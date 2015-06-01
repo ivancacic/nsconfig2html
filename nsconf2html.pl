8136298cca777585d3462e365d8dcad655ba7683
evildani <evil.dani@gmail.com>
2015-06-01 14:26:27 -0500

    Fixed a bug when a VS has no services and added support for traffic domains

--- nsconf2html.pl
+++ nsconf2html.pl
@@ -29,22 +29,80 @@ if($file !~ /conf/){
 die "Not a valid argumet\n";
 }
 
+my %td = ();
+my $has_td = 0;
+my %ips = ();
 my %server = ();
 my %service = ();
 my %vserver = ();
 my %bindings = ();
-#$bindings{"TEST"} = [];
 
 my $out;
 open($out, ">" ,"conf.html") or die "Cloud not open output file\n";
-open my $info, $file or die "Could not open $file: $!";
+
+
+
+
 
 print $out "<html><head><h2>Currently only LB config is displayed in html table format<h2></head><body>";
+#first pass to detect servers
+print "IP list:\n";
+open my $info, $file or die "Could not open $file: $!";
+print $out "<table border=1pt><tr><td>IP</td><td>Mask</td></tr>\n";
+while( my $line = <$info>)  {   
+   
+    if($line =~ /add ns ip/){
+    	my @values = split(' ',$line);
+    	$ips{ $values[3] } = $line; #3 es IP, 4 es netmask y 5 en aldelante son parms
+        print $values[3]."\n";
+        print $out "<tr><td>".$values[3]."</td>";
+        print $out "<td>".$values[4]."</td><tr>\n";
+        }
+}
+print $out "</table><br><br>\n";
+close $info;
 
+print "Traffic Domains:\n";
+open $info, $file or die "Could not open $file: $!";
+print $out "<table border=1pt><tr><td>Traffic Domain</td><td>Alias</td></tr>\n";
+while( my $line = <$info>)  {   
+   
+    if($line =~ /add ns trafficDomain/){
+    	my @values = split(' ',$line);
+    	$td{ $values[3] } = $line; #3 es id, 5 es alias
+        print "ID: ".$values[3]." Alias: ".$values[5]."\n";
+        print $out "<tr><td>".$values[3]."</td>";
+        print $out "<td>".$values[5]."</td><tr>\n";
+        $has_td = 1;
+    }
+}
+print $out "</table><br><br>\n";
+close $info;
+
+
+open $info, $file or die "Could not open $file: $!";
+print $out "<table border=1pt><tr><td>Node ID</td><td>IP</td></tr>\n";
+while( my $line = <$info>)  {   
+   
+    if($line =~ /add HA node/){
+    	my @values = split(' ',$line);
+        print "HA Node ".$values[4]."\n";
+        print $out "<tr><td>".$values[3]."</td>";
+        print $out "<td>".$values[4]."</td><tr>\n";
+        }
+}
+print $out "</table><br><br>\n";
+close $info;
+
+
+open $info, $file or die "Could not open $file: $!";
 #first pass to detect servers
 print "Server list:\n";
-
-print $out "<table border=1pt><tr><td>Server Name</td><td>IP</td></tr>\n";
+print $out "<table border=1pt><tr><td>Server Name</td><td>IP</td>";
+if($has_td==1){
+print $out "<td>TD</td>";
+}
+print $out "</tr>\n";
 while( my $line = <$info>)  {   
    
     if($line =~ /add server/){
@@ -52,26 +110,39 @@ while( my $line = <$info>)  {
     	$server{ $values[2] } = $line;
         print $values[2]."\n";
         print $out "<tr><td>".$values[2]."</td>";
-        print $out "<td>".$values[3]."</td><tr>\n";
+        print $out "<td>".$values[3]."</td>";
+        if($has_td==1)
+        {
+        print $out "<td>".$values[5]."</td>";
+        }
+        print $out "<tr>\n";
         }
 }
 print $out "</table><br><br>\n";
-
 close $info;
 open $info, $file or die "Could not open $file: $!";
 
 print "Service List:\n";
-print $out "<table border=1pt><tr><td>Service Name</td><td>Server</td><td>Port</td></tr>";
+print $out "<table border=1pt><tr><td>Service Name</td><td>Server</td><td>Port</td>";
+if($has_td==1){
+print $out "<td>TD</td>";
+}
+print $out "</tr>";
 while( my $line = <$info>)  {   
    
     if($line =~ /add service/){
     	my @values = split(' ',$line);
+    	my %svc_params = extract_params($line);
     	$service{ $values[2] } = $line;
         print $values[2]."\n";
         print $out "<tr><td>".$values[2]."</td>";
         print $out "<td>".$values[3]."</td>";
         #"<td>".$values[4]."</td>"; this is the type of service
-        print $out "<td>".$values[5]."</td></tr>\n";
+        print $out "<td>".$values[5]."</td>";
+        if($has_td==1){
+			print $out "<td>".$svc_params{"td"}."</td>";
+		}
+        print $out "</tr>\n";
         }
 }
 print $out "</table><br><br>\n";
@@ -81,7 +152,7 @@ close $info;
 open $info, $file or die "Could not open $file: $!";
 
 
-print "Virtual Server - Services:\n";
+print "Bindings for Virtual Server - Services:\n";
 #first pass to detect services bound to virtual servers 
 while( my $line = <$info>)  {   
    
@@ -130,15 +201,36 @@ print $out "<table border=1pt><tr><td>Virtual Server Name</td><td>Category</td><
 while( my $line = <$info>)  {   
    
     if($line =~ /add lb vserver/){
+    	print "START ".$line."\n";
         my @values = split(' ',$line);
         my %params = extract_params($line);
     	$vserver{ $values[3] } = $line;
-    	my @services =  @{$bindings{$values[3]}};
+    	my @services = (); 
+    	my $count_services = 0;
+    	if(exists $bindings{$values[3]}){   	
+    		$count_services = scalar @{$bindings{$values[3]}}."\n";
+    	}
+    	print "VS: ".$values[3]." #servicios ".$count_services."\n";
+    	if($count_services>0){
+    	   	@services =  @{$bindings{$values[3]}};
+    	}
+    	else{
+    		print "VS: ".$values[3]." NO TIENE SERVICIOS\n";
+    	}
     	#print "Dump arr".Dumper(@services)."\n";
 
     	#print "==============\n".Dumper(@services)."\n+++++++\n";
         print $values[3]."\n";
-        my $rowspan = 6 + scalar @services;
+        ##### determine how many lines will be printed#######
+        my $rowspan = 6;
+        if(exists $params{"timeout"})
+    	{ $rowspan++;}
+    	if(exists $params{"lbmethod"})
+    	{ $rowspan++}
+    	if(exists $params{"td"})
+    	{ $rowspan++}
+        #####################################################
+        $rowspan += scalar @services;
         print $out "<tr><td rowspan=".$rowspan.">".$values[3]."</td><td>Tipo</td><td>".$values[4]."</td></tr>\n";
         print $out "<tr><td>IP</td><td>".$values[5]."</td></tr>\n";
         print $out "<tr><td>Puerto</td><td>".$values[6]."</td></tr>\n";
@@ -157,6 +249,11 @@ while( my $line = <$info>)  {
     	}else{
     		print $out "<tr><td>Loadbalance Method</td><td>ROUNDROBIN</td></tr>\n";
     	}
+    	if(exists $params{"td"})
+    	{   		
+    		print $out "<tr><td>Traffic Domain</td><td>".$params{"td"}."</td></tr>\n";
+    	}
+    	
     	
     	#print "\n++++++++++\n".Dumper(@services)."\n==========\n";
     	for my $i (0 .. @services-1){
@@ -168,11 +265,84 @@ while( my $line = <$info>)  {
     		}
     	}
     	#print "Dump hash".Dumper(\%params)."\n";
+    	#persistenceType
+    	#timeout
+    	#cltTimeout
+    	print "END\n";
+    }
+}
+print $out "</table><br><br>\n";
+
+#diffrent format for word paste
+open $info, $file or die "Could not open $file: $!";
+
+print "Virtual Server List:\n";
+print $out "Easy copy paste version of VS list:<br><table border=1pt><tr><td>Virtual Server Name</td><td>Category</td><td>Value</td></tr>\n";
+#first pass to detect virtual servers
+while( my $line = <$info>)  {   
+   
+    if($line =~ /add lb vserver/){
+        my @values = split(' ',$line);
+        my %params = extract_params($line);
+    	$vserver{ $values[3] } = $line;
+    	my @services = (); 
+    	my $count_services = 0;
+    	if(exists $bindings{$values[3]}){   	
+    		$count_services = scalar @{$bindings{$values[3]}}."\n";
+    	}
+    	print "VS: ".$values[3]." #servicios ".$count_services."\n";
+    	if($count_services>0){
+    	   	@services =  @{$bindings{$values[3]}};
+    	}
+    	else{
+    		print "VS: ".$values[3]." NO TIENE SERVICIOS\n";
+    	}
+    	#print "Dump arr".Dumper(@services)."\n";
+
+    	#print "==============\n".Dumper(@services)."\n+++++++\n";
+        print $values[3]."\n";
+        my $rowspan = 6;
+        if(exists $params{"td"}){$rowspan++;}
+        $rowspan +=scalar @services;
+        print $out "<tr><td>".$values[3]."</td><td>Tipo</td><td>".$values[4]."</td></tr>\n";
+        print $out "<tr><td>vs_borrar</td><td>IP</td><td>".$values[5]."</td></tr>\n";
+        print $out "<tr><td>vs_borrar</td><td>Puerto</td><td>".$values[6]."</td></tr>\n";
+    	############## Add aditional lines if you need more rows with information, 
+    	############## params is a hash that uses the key as the -param i.e. -persistenceType without the '-' 
+    	print $out "<tr><<td>vs_borrar</td><td>persistenceType</td><td>".$params{"persistenceType"}."</td></tr>\n";
+    	if(exists $params{"timeout"})
+    	{ 
+    	 	print $out "<tr><td>vs_borrar</td><td>Persistence Timeout</td><td>".$params{"timeout"}."</td></tr>\n";
+    	 }else{
+    	 	print $out "<tr><td>vs_borrar</td><td>Persistence Timeout</td><td>Default</td></tr>\n";
+    	 }
+    	if(exists $params{"lbmethod"})
+    	{ 
+    		print $out "<tr><td>vs_borrar</td><td>Loadbalance Method</td><td>".$params{"lbmethod"}."</td></tr>\n";
+    	}else{
+    		print $out "<tr><td>vs_borrar</td><td>Loadbalance Method</td><td>ROUNDROBIN</td></tr>\n";
+    	}
+    	if(exists $params{"td"})
+    	{   		
+    		print $out "<tr><td>vs_borrar</td><td>Traffic Domain</td><td>".$params{"td"}."</td></tr>\n";
+    	}
+    	
+    	#print "\n++++++++++\n".Dumper(@services)."\n==========\n";
+    	for my $i (0 .. @services-1){
+    		#print "iteracion: ".$i." VS: ".$values[3]." Services: ".@services[$i]."\n";
+    		if ($i==0){ 
+    			print $out "<tr><<td>vs_borrar</td><td>Services</td><td>".$services[$i]."</td></tr>\n";
+    		}else{
+    			print $out "<tr><td>vs_borrar</td><td>vs_borrar</td><td>".$services[$i]."</td></tr>\n";	
+    		}
+    	}
+    	#print "Dump hash".Dumper(\%params)."\n";
     #persistenceType
     #timeout
     #cltTimeout
     }
 }
 print $out "</table><br><br>\n";
+
 
 close $info;
